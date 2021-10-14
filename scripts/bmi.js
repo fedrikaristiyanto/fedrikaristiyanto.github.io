ef8764fe399d6eb6acbdfbc8ca35a2110c5b610a
John <mbahglendang@gmail.com>
2021-10-14 13:53:41 +0700

    Update bmi.js

--- scripts/bmi.js
+++ scripts/bmi.js
@@ -4,5 +4,10 @@ var Hm = document.getElementById("height").value/100;
 var bmi = W / (Hm**2);
 
 if (bmi < 18.5) {
- document.getElementById("output").innerHTML=BMI: " + bmi + "<br> kurus";}
+ document.getElementById("output").innerHTML="BMI: " + bmi + "<br> kurus";}
+elseif (bmi < 25) {
+ document.getElementById("output").innerHTML="BMI: " + bmi + "<br> normal";}
+else {
+document.getElementById("output").innerHTML="BMI: " + bmi + "<br> gemuk";}
+}
 }
