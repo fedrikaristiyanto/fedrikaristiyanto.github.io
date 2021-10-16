199e607750ed9b13cd982e2290aee34dba6088d2
John <mbahglendang@gmail.com>
2021-10-17 06:58:38 +0700

    Update bmi.js

--- scripts/bmi.js
+++ scripts/bmi.js
@@ -1,5 +1,5 @@
 function hitung() {
 var W = document.getElementById("weight").value;
 var Hm = document.getElementById("height").value/100;
-var bmi = document.getElementById("output").innerHTML=W / (Hm**2);
+var bmi = document.getElementById("BMI").value=W / (Hm**2);
 }
