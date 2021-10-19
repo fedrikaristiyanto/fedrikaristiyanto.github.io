09887120724b96b6efc1dc09c1287fd3751b257c
John <mbahglendang@gmail.com>
2021-10-19 07:55:13 +0700

    Update c.js

--- scripts/c.js
+++ scripts/c.js
@@ -1,4 +1,5 @@
 function myFunc() {
-var r = document.getElementById("rad").value;
+var r = Number(document.getElementById("rad").value);
 document.getElementById("area").value=Math.PI * (r**2);
+document.getElementById("circumference").value=2*Math.PI*r;
 }
