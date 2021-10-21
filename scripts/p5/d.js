44a4cbee157760a46e32d120ccb24a8732239dfa
John <mbahglendang@gmail.com>
2021-10-22 02:32:07 +0700

    Update d.js

--- scripts/p5/d.js
+++ scripts/p5/d.js
@@ -1,12 +1,11 @@
-function setup () {
-var canvas = createCanvas(400,400);
-canvas.parent("tgl");
+function setup() {
+  let canvas = createCanvas(100, 100);
+  canvas.parent("tgl");
 }
 
 function draw() {
-background(silver);
-var d = day();
-var m = month();
-var y = year();
-text(d, 5,50);
+  background("silver");
+  let d = day();
+  text('Current day: \n' + d, 5, 50);
+  
 }
