3c4bbe669fc619042bfca5d6fa689427684eea9e
John <mbahglendang@gmail.com>
2021-10-24 23:40:30 +0700

    Update randomColor.js

--- scripts/randomColor.js
+++ scripts/randomColor.js
@@ -1,1 +1,25 @@
-function(){}
+function change(){
+ let x = Math.floor(Math.random()*6);
+ 
+ let color;
+ if (x==0){
+  color = "Red";
+ }
+ else if (x==1){
+  color = "Green";
+ }
+ else if (x==2){
+  color = "Blue";
+ }
+ else if (x==3){
+  color = "Cyan";
+ }
+ else if (x==4){
+  color = "Magenta";
+ }
+ else {
+  color = "Yellow";
+ }
+document.body.style.backgroundColor=color;
+document.getElementById("val").value=x;
+}
