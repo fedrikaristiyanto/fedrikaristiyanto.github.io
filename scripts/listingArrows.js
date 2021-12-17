bb6595ea7cdc61522ce4b7d548e3773212f970b6
John <mbahglendang@gmail.com>
2021-12-17 18:47:22 +0700

    Update listingArrows.js

--- scripts/listingArrows.js
+++ scripts/listingArrows.js
@@ -1,7 +1,7 @@
 function listingArrows() {
 let text = "";
 for (let i = 8592; i <= 8703; i++) {
-  text += String.fromCharCode(i) + "\n";
+  text += i + ":" + String.fromCharCode(i) + "\n";
 }
 
 document.getElementById("list").value = text;
