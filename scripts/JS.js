589908b7c6938ae0e29ec4b9d114e787c27fc7cc
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-01-25 00:21:56 +0700

    Update JS.js

--- scripts/JS.js
+++ scripts/JS.js
@@ -1,5 +1,10 @@
 function GanjilGenap(){
 var number = parseInt(document.getElementById("inp").value);  
 var answer = (number % 2 == 1) ? ' adalah bilangan Ganjil' : ' adalah bilangan Genap';  
+if (isNaN(number){
+ alert("not valid");
+}
+else {
 document.getElementById("outp").value=number + answer;
+}
 }
