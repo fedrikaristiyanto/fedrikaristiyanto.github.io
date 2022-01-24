d81915eaf489dbe43edc339ac7d905c3b7975885
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-01-24 23:30:06 +0700

    Update JS.js

--- scripts/JS.js
+++ scripts/JS.js
@@ -1,3 +1,5 @@
-function ...() {
-
+function GanjilGenap(){
+var number = parseInt(document.getElementById("inp").value);  
+var answer = (number % 2 == 1) ? ' adalah bilangan Ganjil' : ' adalah bilangan Genap';  
+document.getElementById("outp").innerHTML=number + answer;
 }
