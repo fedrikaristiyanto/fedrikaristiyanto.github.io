6a537c1c7e8fb32840d9600c8302f886f16063be
fedrikaristiyanto <mbahglendang@gmail.com>
2022-01-25 08:46:00 +0700

    Update JS.js

--- scripts/JS.js
+++ scripts/JS.js
@@ -1,8 +1,9 @@
+//untuk mengecek bilangan ganjil atau genap
 function GanjilGenap(){
 var number = parseInt(document.getElementById("inp").value);  
 var answer = (number % 2 == 1) ? ' adalah bilangan Ganjil' : ' adalah bilangan Genap';  
 if (isNaN(number)){
- alert("not valid");
+ alert("input not valid");
 }
 else {
 document.getElementById("outp").value=number + answer;
