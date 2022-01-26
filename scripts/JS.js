f57756de29f88b46a999f8750a990a480b51604d
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-01-26 16:46:51 +0700

    Update JS.js

--- scripts/JS.js
+++ scripts/JS.js
@@ -1,11 +1,16 @@
 //untuk mengecek bilangan ganjil atau genap
 function GanjilGenap(){
-var number = parseInt(document.getElementById("inp").value);  
-var answer = (number % 2 == 1) ? ' adalah bilangan Ganjil' : ' adalah bilangan Genap';  
+ var number = parseInt(document.getElementById("inp").value);  
+ var answer = (number % 2 == 1) ? ' adalah bilangan Ganjil' : ' adalah bilangan Genap';  
 if (isNaN(number)){
  alert("input not valid");
 }
 else {
 document.getElementById("outp").value=number + answer;
 }
+}
+
+// what is my screen resolution
+function MyScreenResolution() {
+ alert('Your screen resolution: ' + screen.width+' x '+screen.height);
 }
