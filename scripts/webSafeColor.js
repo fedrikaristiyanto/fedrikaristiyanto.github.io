ccdd57f2d01acbb3980c03ec654a8c164fe8ca50
fedrikaristiyanto <mbahglendang@gmail.com>
2022-02-08 23:20:26 +0700

    Create webSafeColor.js

---
+++ scripts/webSafeColor.js
@@ -0,0 +1,12 @@
+function webSafeColor{
+var Inp1 = parseInt(document.getElementById("rangeInp1").value);
+var Inp2 = parseInt(document.getElementById("rangeInp2").value);
+var Inp3 = parseInt(document.getElementById("rangeInp3").value);
+var web_safe_color = document.getElementById("val").value=
+"#" + Inp1.toString(16).toUpperCase()
+    + Inp2.toString(16).toUpperCase()
+    + Inp3.toString(16).toUpperCase();
+
+document.getElementById("pratinjau").style.backgroundColor=
+web_safe_color;
+}
