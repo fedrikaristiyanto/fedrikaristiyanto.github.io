7c5425948a7b8a9bf30333f5e195ddcc50f5e6b3
fedrikaristiyanto <mbahglendang@gmail.com>
2022-02-11 12:40:41 +0700

    Update clearinput.js

--- scripts/clearinput.js
+++ scripts/clearinput.js
@@ -1,11 +1,14 @@
+//single clear input field 
 function cl(){
   document.getElementById("inp").value="";
 }
 
-function cl2(){
- var FL = document.querySelectorAll(".field");
 
-for (var i = 0; i < FL.length; i++) {
-  FL[i].value = "";}
+//multi clear input field with class name .field
+function cl2(){
+ var FLD = document.querySelectorAll(".field");
+
+ for (var i = 0; i < FLD.length; i++) {
+   FLD[i].value = "";}
 
 }
