4ba57544e49eb55b89767b481456f044b8f51136
fedrikaristiyanto <mbahglendang@gmail.com>
2022-02-22 23:59:04 +0700

    Update Lnk_persegiPanjang.js

--- scripts/Lnk_persegiPanjang.js
+++ scripts/Lnk_persegiPanjang.js
@@ -8,7 +8,7 @@ var L = p*l;
 var k = 2*(p+l);
 
 if (l >= p || p=="" || l=="") {
-  alert("tdk msk akal, l tdk lbh pjg dr p ato sama dgn p \n\npastikan smw bidang tlah diisi");
+  alert("tdk msk akal, l tdk lbh pjg dr p ato sama dgn p \n& \npastikan smw bidang tlah diisi");
 }
 else{
 document.getElementById("Result").value=`Luas : ${L} 
