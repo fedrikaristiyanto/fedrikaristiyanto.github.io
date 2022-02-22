1396dc09741bddb68079875b2dfc776b92d8416c
fedrikaristiyanto <mbahglendang@gmail.com>
2022-02-22 15:26:34 +0700

    Update Lnk_persegiPanjang.js

--- scripts/Lnk_persegiPanjang.js
+++ scripts/Lnk_persegiPanjang.js
@@ -2,10 +2,10 @@ var Button = document.getElementById("bttn");
   Button.addEventListener("click", hitung);
 
 function hitung(){
-var p = document.getElementById("pjg").value;
-var l = document.getElementById("lbr").value;
+var p = Number(document.getElementById("pjg").value);
+var l = Number(document.getElementById("lbr").value);
 var L = p*l;
-var k = (2*p)+(2*l);
+var k = 2*(p+l);
 
 if (l >= p || p=="" || l=="") {
   alert("tdk msk akal, l tdk lbh pjg dr p ato sama dgn p \n\npastikan smw bidang tlah diisi");
