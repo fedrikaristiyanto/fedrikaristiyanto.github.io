78ec7d993a2c63afdd6a895e94bd612b499e04b4
fedrikaristiyanto <mbahglendang@gmail.com>
2022-03-01 18:48:30 +0700

    Update styling.js

--- scripts/styling.js
+++ scripts/styling.js
@@ -2,3 +2,18 @@ function switchColor() {
  var elemen = document.querySelector("body");
  elemen.classList.toggle("DarkMode");
 }
+
+
+function changeTxt(){
+  
+var stl = document.form.syle.value;
+if (stl == "Uppercase"){
+document.getElementById("txt").style.textTransform="uppercase";
+}
+else if (stl == "Lowercase"){
+document.getElementById("txt").style.textTransform="lowercase";
+}
+else {
+document.getElementById("txt").style.textTransform="capitalize";
+}
+}
