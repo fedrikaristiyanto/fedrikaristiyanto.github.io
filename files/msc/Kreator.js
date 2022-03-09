e3715c63060911aeeb503f27d46083dd72ca5b41
fedrikaristiyanto <mbahglendang@gmail.com>
2022-03-09 08:17:28 +0700

    Update Kreator.js

--- files/msc/Kreator.js
+++ files/msc/Kreator.js
@@ -1,7 +1,6 @@
 function Load(){
 var HordesOfChaos = [
  "<a href='
-https://drive.google.com/file/d/1ugpDh5YKOraqgB3pepLIutxeURaRgXKB/view?usp=sharing'>Hordes Of Chaos</a>",
- "War Curse"];
+https://drive.google.com/file/d/1ugpDh5YKOraqgB3pepLIutxeURaRgXKB/view?usp=sharing'>Hordes Of Chaos</a>", "War Curse"];
 document.getElementById("HordesOfChaos").innerHTML=HordesOfChaos.join(" <br> ");
 }
