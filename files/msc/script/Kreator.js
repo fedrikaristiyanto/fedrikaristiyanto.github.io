4ab5638dc7d72f9c43023de0922a3be0d103f489
fedrikaristiyanto <mbahglendang@gmail.com>
2022-03-09 16:14:30 +0700

    Update Kreator.js

--- files/msc/script/Kreator.js
+++ files/msc/script/Kreator.js
@@ -2,7 +2,7 @@ function LoadMsc(){
 var HordesOfChaos = [
 "<a href='http://docs.google.com/uc?export=open&id=1ugpDh5YKOraqgB3pepLIutxeURaRgXKB'>Hordes Of Chaos</a>", 
 "<a href='http://docs.google.com/uc?export=open&id=1rM825C8nWU7z-1cpAAX9wimaxdSBPzHU'>War Curse</a>", 
-"Escalation", 
+"<a href='http://docs.google.com/uc?export=open&id=1XL1iVT0kXU9CzKZdxrSOz2gAsXjyaXb-'>Escalation</a>", 
 "Amok Run"
 ];
 document.getElementById("HordesOfChaos").innerHTML=HordesOfChaos.join(" <br> ");
