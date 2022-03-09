ce76ab11785ce75e2188536ec625ae60580d87de
fedrikaristiyanto <mbahglendang@gmail.com>
2022-03-09 08:37:48 +0700

    Update Kreator.js
    fix

--- files/msc/Kreator.js
+++ files/msc/Kreator.js
@@ -3,3 +3,4 @@ var HordesOfChaos = [
  "<a href='http://docs.google.com/uc?export=open&id=1ugpDh5YKOraqgB3pepLIutxeURaRgXKB'>Hordes Of Chaos</a>" , "War Curse"];
 document.getElementById("HordesOfChaos").innerHTML=HordesOfChaos.join(" <br> ");
 }
+
