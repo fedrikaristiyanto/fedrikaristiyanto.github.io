81c9f98b1403e1f78a0c7c6cac9f42123ac481b0
fedrikaristiyanto <mbahglendang@gmail.com>
2022-03-09 08:31:38 +0700

    Update Kreator.js

--- files/msc/Kreator.js
+++ files/msc/Kreator.js
@@ -1,6 +1,5 @@
 function LoadMsc(){
 var HordesOfChaos = [
- "<a href='
-https://drive.google.com/file/d/1ugpDh5YKOraqgB3pepLIutxeURaRgXKB/view?usp=sharing'>Hordes Of Chaos</a>", "War Curse"];
+ "<a href='#1ugpDh5YKOraqgB3pepLIutxeURaRgXKB'>Hordes Of Chaos</a>" , "War Curse"];
 document.getElementById("HordesOfChaos").innerHTML=HordesOfChaos.join(" <br> ");
 }
