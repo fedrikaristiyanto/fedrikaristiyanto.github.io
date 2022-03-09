0317bea46cb2da4422570bb0376547b32a3f11dc
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-03-09 17:24:51 +0700

    Update Metallica_Unforgiven.js

--- files/msc/script/Metallica_Unforgiven.js
+++ files/msc/script/Metallica_Unforgiven.js
@@ -1,6 +1,16 @@
-var unforgiven = [
+function loaD(){
+const unforgiven = [
 "<a href='http://docs.google.com/uc?export=open&id=1CNONAQia2CWajZZ8_fA6-eTF_ryRxtoH'>The Unforgiven</a>",
 "<a href='http://docs.google.com/uc?export=open&id=1LWBbF7oxJV7im8Cd48ghBCIOuA6WjyET'>The Unforgiven II</a>",
 "<a href='http://docs.google.com/uc?export=open&id=1VUb18AXP79n7-WzJc2wY6L2hY1yaEhlZ'>The Unforgiven III</a>"
 ];
-document.getElementById("Unforgiven").innerHTML=unforgiven.join(" <br> ");
+
+let text = "<ul>";
+
+for (let i = 0; i < unforgiven.length; i++){
+    
+    text += "<li>" + unforgiven[i]  + "</li>";
+}
+text += "</ul>"
+document.getElementById("Unforgiven").innerHTML=text;
+}
