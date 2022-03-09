4b9a88fee16c51fa8bab6b93068d6bb397bad7f8
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-03-09 09:46:25 +0700

    Update Metallica_Unforgiven.js

--- files/msc/script/Metallica_Unforgiven.js
+++ files/msc/script/Metallica_Unforgiven.js
@@ -1,2 +1,4 @@
-var unforgiven = [ ,, ];
+var unforgiven = [
+"<a href='http://docs.google.com/uc?export=open&id= '>Unforgiven</a>", 
+,, ];
 document.getElementById("Unforgiven").innerHTML=unforgiven.join(" ");
