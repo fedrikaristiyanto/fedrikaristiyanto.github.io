bacb7834d358f7a8e803855860428d92110aaa59
fedrikaristiyanto <mbahglendang@gmail.com>
2022-03-10 09:19:48 +0700

    Update various.js

--- files/msc/script/various.js
+++ files/msc/script/various.js
@@ -1,5 +1,5 @@
 var ious = [
-"George Benson - Breezin",
+"<a href='http://docs.google.com/uc?export=open&id=1KfV-TQeMgWmd8AMZ0WyvaFSq-6ijdH0z'>George Benson - Breezin</a>",
 "Whitney Houston - I will always love You"
 ];
 document.getElementById("various").innerHTML=ious.join(" <br> ");
