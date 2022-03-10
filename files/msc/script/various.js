9eb183c921b07479275707053537eaedbdcede36
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-03-10 09:27:37 +0700

    Update various.js

--- files/msc/script/various.js
+++ files/msc/script/various.js
@@ -1,5 +1,5 @@
 var ious = [
 "<a href='http://docs.google.com/uc?export=open&id=1KfV-TQeMgWmd8AMZ0WyvaFSq-6ijdH0z'>George Benson - Breezin</a>",
-"Whitney Houston - I will always love You"
+"<a href='http://docs.google.com/uc?export=open&id=19-2z0UTM3_4u0r4aUOcV5bbA4I3vmF6n'>Whitney Houston - I Will Always Love You</a>"
 ];
 document.getElementById("various").innerHTML=ious.join(" <br> ");
