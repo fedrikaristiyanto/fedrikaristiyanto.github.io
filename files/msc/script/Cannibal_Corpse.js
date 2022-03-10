f494725e25862d98a2bb5ed46553360fdc641c8c
fedrikaristiyanto <mbahglendang@gmail.com>
2022-03-10 11:41:13 +0700

    Update Cannibal_Corpse.js

--- files/msc/script/Cannibal_Corpse.js
+++ files/msc/script/Cannibal_Corpse.js
@@ -1,5 +1,5 @@
 var ep = [
-"<a href='http://docs.google.com/uc?export=open&id=1DA1zn4pJe1sGUEIhAQwoQnXzqKxNtCwP'>Priests of Sodom</a>"
-
+"<a href='http://docs.google.com/uc?export=open&id=1DA1zn4pJe1sGUEIhAQwoQnXzqKxNtCwP'>Priests of Sodom</a>",
+"<a href='http://docs.google.com/uc?export=open&id=1LzxNr0Siwqy9oGcu860U7cgzi9unaW1N'>Scalding Hail</a>"
 ];
 document.getElementById("Evisceration_Plague").innerHTML=ep;
