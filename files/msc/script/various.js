0bf8361722d1c1053b3f2fda1c5844bca130128e
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-03-13 11:31:04 +0700

    Update various.js

--- files/msc/script/various.js
+++ files/msc/script/various.js
@@ -1,6 +1,6 @@
 var ious = [
 "<a href='http://docs.google.com/uc?export=open&id=1KfV-TQeMgWmd8AMZ0WyvaFSq-6ijdH0z'>George Benson - Breezin</a>",
-"<a href='http://docs.google.com/uc?export=1cn0qopOfXec9Gw1vgpd_jNmwfuqunFvY'>Onnanoko WA daredemo</a>", 
+"<a href='http://docs.google.com/uc?export=open&id=1cn0qopOfXec9Gw1vgpd_jNmwfuqunFvY'>Onnanoko WA daredemo</a>", 
 "<a href='http://docs.google.com/uc?export=open&id=19-2z0UTM3_4u0r4aUOcV5bbA4I3vmF6n'>Whitney Houston - I Will Always Love You</a>"
 ];
 document.getElementById("various").innerHTML=ious.join(" <br> ");
