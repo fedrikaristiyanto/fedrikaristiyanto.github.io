4257a5cbedf6898552d846ad3ad75b9ca77ca61b
fedrikaristiyanto <mbahglendang@gmail.com>
2022-03-14 11:23:09 +0700

    Update Cannibal_Corpse.js

--- files/msc/script/Cannibal_Corpse.js
+++ files/msc/script/Cannibal_Corpse.js
@@ -4,6 +4,7 @@ var ep = [
 "<a href='http://docs.google.com/uc?export=open&id=193AXPTNeljh1XXqkNpv9EiG8NMnuf0My'>To Decompose</a>",
 "<a href='http://docs.google.com/uc?export=open&id=1d6Aaukd24vUr4nr6VVn9EgdKs4EZg5T1'>A Cauldron of Hate</a>",
 "<a href='http://docs.google.com/uc?export=open&id=1wfgqdQDD7rfePX4F5n76D24y8ya18mYz'>Beheading and Burning</a>",
+"<a href='http://docs.google.com/uc?export=open&id=1SkbSzwBLAly_RpI5xNK2jCwwJoCO2Rnk'>Evidence in the Furnace</a>",
 "<a href='http://docs.google.com/uc?export=open&id=1hVznbcDUvYMkCydBV0pmM43mgJujFcUU'>Evisceration Plague</a>"
 ];
 document.getElementById("Evisceration_Plague").innerHTML=ep.join(" <br> ");
