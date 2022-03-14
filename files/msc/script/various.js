f44be72040de9071f2846578c914045e5ff69a2e
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-03-14 09:07:10 +0700

    Update various.js

--- files/msc/script/various.js
+++ files/msc/script/various.js
@@ -1,6 +1,6 @@
 var ious = [
 "<a href='http://docs.google.com/uc?export=open&id=1KfV-TQeMgWmd8AMZ0WyvaFSq-6ijdH0z'>George Benson - Breezin</a>",
-"<a href'http://docs.google.com/uc?export=open&id=1nDyEm_bYPimdwwtZVuQyUwdi7OLsF0M6'>Marilyn Monroe - I wanna be loved by you</a>", 
+"<a href='http://docs.google.com/uc?export=open&id=1nDyEm_bYPimdwwtZVuQyUwdi7OLsF0M6'>Marilyn Monroe - I wanna be loved by you</a>", 
 "<a href='http://docs.google.com/uc?export=open&id=1cn0qopOfXec9Gw1vgpd_jNmwfuqunFvY'>Tokyo Jihen - Onnanoko WA daredemo</a>", 
 "<a href='http://docs.google.com/uc?export=open&id=19-2z0UTM3_4u0r4aUOcV5bbA4I3vmF6n'>Whitney Houston - I Will Always Love You</a>"
 ];
