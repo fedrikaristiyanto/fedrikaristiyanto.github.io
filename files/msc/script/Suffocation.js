62784740099a88d88e5f483a2639878461d25017
fedrikaristiyanto <mbahglendang@gmail.com>
2022-03-15 12:23:48 +0700

    Update Suffocation.js

--- files/msc/script/Suffocation.js
+++ files/msc/script/Suffocation.js
@@ -1,6 +1,4 @@
-var std = ["
-
-1RiVJPxk6oKufRmVGWS4Jy7P2agdkvcjt
-
-"];
+var std = [
+"<a href='http://docs.google.com/uc?export=open&id=1RiVJPxk6oKufRmVGWS4Jy7P2agdkvcjt'>Deceit</a>"
+];
 document.getElementById("STD").innerHTML=std.join(" <br> ");
