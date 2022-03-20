7975fc4ca4add1aed386bd5e53392aff078cc2cd
fedrikaristiyanto <mbahglendang@gmail.com>
2022-03-20 09:47:47 +0700

    Update Suffocation.js

--- files/msc/script/Suffocation.js
+++ files/msc/script/Suffocation.js
@@ -1,5 +1,5 @@
 var std = [
-"<a href='http://docs.google.com/uc?export=open&id=1RiVJPxk6oKufRmVGWS4Jy7P2agdkvcjt'>Deceit</a>"
+"<a href='http://docs.google.com/uc?export=open&id=1RiVJPxk6oKufRmVGWS4Jy7P2agdkvcjt'>Deceit</a>",
 "<a href='http://docs.google.com/uc?export=open&id=1wTI-K9_RPSfG1v6I_BRutJH_JMlEkjlk'>To Weep Once More</a>"
 ];
 document.getElementById("STD").innerHTML=std.join(" <br> ");
