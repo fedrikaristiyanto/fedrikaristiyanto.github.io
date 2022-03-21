45407e2532bd6d33c7b4657b87c2c42fb53f12e8
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-03-21 09:28:57 +0700

    Update Metallica.js

--- files/msc/script/Metallica.js
+++ files/msc/script/Metallica.js
@@ -1,2 +1,5 @@
-var mtlc = [];
+var mtlc = [
+"<a href='https://drive.google.com/file/d/1-Az5FUdjWTR4k-BTXGXogvHBCNByJJa3/view?usp=sharing'>
+Metallica - Enter Sandman</a>"
+];
 document.getElementById("Mtlc").innerHTML=mtlc;
