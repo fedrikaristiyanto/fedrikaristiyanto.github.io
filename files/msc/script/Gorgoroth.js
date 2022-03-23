d61f826c0ddafb3f2a9038dc5de57fc8c5b49536
fedrikaristiyanto <mbahglendang@gmail.com>
2022-03-23 10:09:13 +0700

    Update Gorgoroth.js

--- files/msc/script/Gorgoroth.js
+++ files/msc/script/Gorgoroth.js
@@ -1,5 +1,5 @@
 var t = [
-"<a href='#'>Procreating Satan</a>",
+"<a href='https://docs.google.com/uc?export=open&id=1YDvaySKkh5Jt-jJVyAI1N5gU9WOLq9_z'>Procreating Satan</a>",
 "Proclaiming Mercy - Damaging Instinct of Man"
 ];
 document.getElementById("Twilight").innerHTML=t;
