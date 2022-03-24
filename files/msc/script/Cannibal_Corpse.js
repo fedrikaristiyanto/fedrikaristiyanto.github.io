191ec703e90dd31671a53570f80818352c4fe972
fedrikaristiyanto <mbahglendang@gmail.com>
2022-03-24 18:45:00 +0700

    Update Cannibal_Corpse.js

--- files/msc/script/Cannibal_Corpse.js
+++ files/msc/script/Cannibal_Corpse.js
@@ -1,12 +1,13 @@
 var ep = [
-"<a href='http://docs.google.com/uc?export=open&id=1DA1zn4pJe1sGUEIhAQwoQnXzqKxNtCwP'>Priests of Sodom</a>",
-"<a href='http://docs.google.com/uc?export=open&id=1LzxNr0Siwqy9oGcu860U7cgzi9unaW1N'>Scalding Hail</a>",
-"<a href='http://docs.google.com/uc?export=open&id=193AXPTNeljh1XXqkNpv9EiG8NMnuf0My'>To Decompose</a>",
-"<a href='http://docs.google.com/uc?export=open&id=1d6Aaukd24vUr4nr6VVn9EgdKs4EZg5T1'>A Cauldron of Hate</a>",
-"<a href='http://docs.google.com/uc?export=open&id=1wfgqdQDD7rfePX4F5n76D24y8ya18mYz'>Beheading and Burning</a>",
-"<a href='http://docs.google.com/uc?export=open&id=1SkbSzwBLAly_RpI5xNK2jCwwJoCO2Rnk'>Evidence in the Furnace</a>",
-"<a href='http://docs.google.com/uc?export=open&id=1iu6cz5Udj4RrvjKfJ6wIqbsDUFqkT90s'>Carnivorous Swarm</a>",
-"<a href='http://docs.google.com/uc?export=open&id=1hVznbcDUvYMkCydBV0pmM43mgJujFcUU'>Evisceration Plague</a>",
-"<a href='http://docs.google.com/uc?export=open&id=177rDNArYCZDDy15ADt99VXckQYe84hcW'>Shatter Their Bones</a>"
+"<a href='https://docs.google.com/uc?export=open&id=1DA1zn4pJe1sGUEIhAQwoQnXzqKxNtCwP'>Priests of Sodom</a>",
+"<a href='https://docs.google.com/uc?export=open&id=1LzxNr0Siwqy9oGcu860U7cgzi9unaW1N'>Scalding Hail</a>",
+"<a href='https://docs.google.com/uc?export=open&id=193AXPTNeljh1XXqkNpv9EiG8NMnuf0My'>To Decompose</a>",
+"<a href='https://docs.google.com/uc?export=open&id=1d6Aaukd24vUr4nr6VVn9EgdKs4EZg5T1'>A Cauldron of Hate</a>",
+"<a href='https://docs.google.com/uc?export=open&id=1wfgqdQDD7rfePX4F5n76D24y8ya18mYz'>Beheading and Burning</a>",
+"<a href='https://docs.google.com/uc?export=open&id=1SkbSzwBLAly_RpI5xNK2jCwwJoCO2Rnk'>Evidence in the Furnace</a>",
+"<a href='https://docs.google.com/uc?export=open&id=1iu6cz5Udj4RrvjKfJ6wIqbsDUFqkT90s'>Carnivorous Swarm</a>",
+"<a href='https://docs.google.com/uc?export=open&id=1hVznbcDUvYMkCydBV0pmM43mgJujFcUU'>Evisceration Plague</a>",
+"<a href='https://docs.google.com/uc?export=open&id=177rDNArYCZDDy15ADt99VXckQYe84hcW'>Shatter Their Bones</a>",
+"<a href='https://docs.google.com/uc?export=open&id=1BdbN5LXKjmn2FX2PqHORj6il9EGvH2cO'>Carrion Sculpted Entity</a>"
 ];
 document.getElementById("Evisceration_Plague").innerHTML=ep.join(" <br> ");
