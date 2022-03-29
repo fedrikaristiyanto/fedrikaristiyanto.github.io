6d8424f377b5d8bf1b70cf1b637a24e1a9cd76ef
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-03-29 19:00:39 +0700

    Update Metallica.js

--- files/msc/script/Metallica.js
+++ files/msc/script/Metallica.js
@@ -1,4 +1,5 @@
 var mtlc = [
-"<a href='http://docs.google.com/uc?export=open&id=1-Az5FUdjWTR4k-BTXGXogvHBCNByJJa3'>Metallica - Enter Sandman</a>"
+"<a href='https://docs.google.com/uc?export=open&id=1-Az5FUdjWTR4k-BTXGXogvHBCNByJJa3'>Metallica - Enter Sandman</a>", 
+"<a href='https://docs.google.com/uc?export=open&id=1lZEwV7oyJySuSxnEZZeGtEUqienkapFi'>Metallica - Sad But True</a>"
 ];
 document.getElementById("Mtlc").innerHTML=mtlc;
