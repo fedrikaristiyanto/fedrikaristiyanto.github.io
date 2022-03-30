ca975de8322bce672aeb9c247a3dca3f116f17f2
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-03-30 10:01:26 +0700

    Update various.js

--- files/msc/script/various.js
+++ files/msc/script/various.js
@@ -5,6 +5,7 @@ var ious = [
 "<a href='http://docs.google.com/uc?export=open&id=14hjEoQc8mYpxV5aJXt9-GNimiXRAAbby'>Lee Ritenour - Stolen Moments</a>", 
 "<a href='http://docs.google.com/uc?export=open&id=1nDyEm_bYPimdwwtZVuQyUwdi7OLsF0M6'>Marilyn Monroe - I wanna be loved by you</a>", 
 "<a href='http://docs.google.com/uc?export=open&id=1cn0qopOfXec9Gw1vgpd_jNmwfuqunFvY'>Tokyo Jihen - Onnanoko WA daredemo</a>", 
-"<a href='http://docs.google.com/uc?export=open&id=19-2z0UTM3_4u0r4aUOcV5bbA4I3vmF6n'>Whitney Houston - I Will Always Love You</a>"
+"<a href='https://docs.google.com/uc?export=open&id=1pBnRe3pa3b-7-VWWXB7gmeJ1HAcR5ii5'>Celine Dion - My Heart Will Go On</a>", 
+"<a href='https://docs.google.com/uc?export=open&id=19-2z0UTM3_4u0r4aUOcV5bbA4I3vmF6n'>Whitney Houston - I Will Always Love You</a>"
 ];
 document.getElementById("various").innerHTML=ious.join(" <br> ");
