10054e8de03af812abb3e50d5cbcc22aca5a9a79
fedrikaristiyanto <mbahglendang@gmail.com>
2022-04-06 00:26:46 +0700

    Update Metallica_Unforgiven.js

--- files/msc/script/Metallica_Unforgiven.js
+++ files/msc/script/Metallica_Unforgiven.js
@@ -1,8 +1,8 @@
 function loaD(){
 const unforgiven = [
-"<a href='https://drive.google.com/uc?id=1CNONAQia2CWajZZ8_fA6-eTF_ryRxtoH&amp;export=download'>The Unforgiven</a>",
+"<a href='https://docs.google.com/uc?id=1CNONAQia2CWajZZ8_fA6-eTF_ryRxtoH&amp;export=download'>The Unforgiven</a>",
 "<a href='https://docs.google.com/uc?id=1LWBbF7oxJV7im8Cd48ghBCIOuA6WjyET&amp;export=download'>The Unforgiven II</a>",
-"<a href='http://docs.google.com/uc?export=open&id=1VUb18AXP79n7-WzJc2wY6L2hY1yaEhlZ' download>The Unforgiven III</a>"
+"<a href='https://docs.google.com/uc?id=1VUb18AXP79n7-WzJc2wY6L2hY1yaEhlZ&amp;export=download'>The Unforgiven III</a>"
 ];
 
 let text = "<ul>";
