39cf3ebc99267183c1718ce072bc721f1b8f0c3a
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-04-06 16:24:15 +0700

    Update Kreator.js

--- files/msc/script/Kreator.js
+++ files/msc/script/Kreator.js
@@ -1,9 +1,9 @@
 function LoadMsc(){
 var HordesOfChaos = [
-"<a href='http://docs.google.com/uc?export=open&id=1ugpDh5YKOraqgB3pepLIutxeURaRgXKB'>Hordes Of Chaos</a>", 
-"<a href='http://docs.google.com/uc?export=open&id=1rM825C8nWU7z-1cpAAX9wimaxdSBPzHU'>War Curse</a>", 
-"<a href='http://docs.google.com/uc?export=open&id=1XL1iVT0kXU9CzKZdxrSOz2gAsXjyaXb-'>Escalation</a>", 
-"<a href='http://docs.google.com/uc?export=open&id=1Ut6qFM8iRt9BjICJ8cAEo5Qw1buDkyhm'>Amok Run</a>"
+"<a href='https://docs.google.com/uc?export=open&id=1ugpDh5YKOraqgB3pepLIutxeURaRgXKB'>Hordes Of Chaos</a>", 
+"<a href='https://docs.google.com/uc?export=open&id=1rM825C8nWU7z-1cpAAX9wimaxdSBPzHU'>War Curse</a>", 
+"<a href='https://docs.google.com/uc?export=open&id=1XL1iVT0kXU9CzKZdxrSOz2gAsXjyaXb-'>Escalation</a>", 
+"<a href='https://docs.google.com/uc?export=open&id=1Ut6qFM8iRt9BjICJ8cAEo5Qw1buDkyhm'>Amok Run</a>"
 ];
 document.getElementById("HordesOfChaos").innerHTML=HordesOfChaos.join(" <br> ");
 }
