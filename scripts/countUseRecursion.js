e4416483ec91033177e281d53dc1c5c96a870775
fedrikaristiyanto <mbahglendang@gmail.com>
2022-04-19 11:20:41 +0700

    Create countUseRecursion.js

---
+++ scripts/countUseRecursion.js
@@ -0,0 +1,18 @@
+/*
+var number=1;
+function count(number) {
+
+    
+    console.log(number);
+
+    
+    var newNumber = number + 1;
+
+    
+    if (newNumber <= 10) {
+        count(newNumber);
+    }
+}
+
+count(number);
+*/
