c89ded2725386fc3aa535c814be0871e5cb9e553
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-06-15 12:35:10 +0700

    Update Cannibal_Corpse.js

--- files/msc/script/Cannibal_Corpse.js
+++ files/msc/script/Cannibal_Corpse.js
@@ -1,6 +1,6 @@
 var ep = [
 "<a href='https://docs.google.com/uc?export=download&id=1DA1zn4pJe1sGUEIhAQwoQnXzqKxNtCwP'>Priests of Sodom</a>",
-"<a href='https://docs.google.com/uc?export=open&id=1LzxNr0Siwqy9oGcu860U7cgzi9unaW1N'>Scalding Hail</a>",
+"<a href='https://docs.google.com/uc?export=download&id=1LzxNr0Siwqy9oGcu860U7cgzi9unaW1N'>Scalding Hail</a>",
 "<a href='https://docs.google.com/uc?export=open&id=193AXPTNeljh1XXqkNpv9EiG8NMnuf0My'>To Decompose</a>",
 "<a href='https://docs.google.com/uc?export=open&id=1d6Aaukd24vUr4nr6VVn9EgdKs4EZg5T1'>A Cauldron of Hate</a>",
 "<a href='https://docs.google.com/uc?export=open&id=1wfgqdQDD7rfePX4F5n76D24y8ya18mYz'>Beheading and Burning</a>",
