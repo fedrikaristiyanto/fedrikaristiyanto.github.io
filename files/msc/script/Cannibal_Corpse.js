6d247cda7e72fc5d938b57879cd98cc0d57791d7
fedrikaristiyanto <mbahglendang@gmail.com>
2022-06-16 09:37:17 +0700

    Update Cannibal_Corpse.js

--- files/msc/script/Cannibal_Corpse.js
+++ files/msc/script/Cannibal_Corpse.js
@@ -4,7 +4,7 @@ var ep = [
 "<a href='https://docs.google.com/uc?export=download&id=193AXPTNeljh1XXqkNpv9EiG8NMnuf0My'>To Decompose</a>",
 "<a href='https://docs.google.com/uc?export=download&id=1d6Aaukd24vUr4nr6VVn9EgdKs4EZg5T1'>A Cauldron of Hate</a>",
 "<a href='https://docs.google.com/uc?export=download&id=1wfgqdQDD7rfePX4F5n76D24y8ya18mYz'>Beheading and Burning</a>",
-"<a href='https://docs.google.com/uc?export=open&id=1SkbSzwBLAly_RpI5xNK2jCwwJoCO2Rnk'>Evidence in the Furnace</a>",
+"<a href='https://docs.google.com/uc?export=download&id=1SkbSzwBLAly_RpI5xNK2jCwwJoCO2Rnk'>Evidence in the Furnace</a>",
 "<a href='https://docs.google.com/uc?export=open&id=1iu6cz5Udj4RrvjKfJ6wIqbsDUFqkT90s'>Carnivorous Swarm</a>",
 "<a href='https://docs.google.com/uc?export=open&id=1hVznbcDUvYMkCydBV0pmM43mgJujFcUU'>Evisceration Plague</a>",
 "<a href='https://docs.google.com/uc?export=open&id=177rDNArYCZDDy15ADt99VXckQYe84hcW'>Shatter Their Bones</a>",
