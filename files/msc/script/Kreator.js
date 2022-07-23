6397074f4062a6ad03ac4ad196b78f62c8ece6f7
fedrikaristiyanto <mbahglendang@gmail.com>
2022-07-23 18:12:24 +0700

    Update Kreator.js

--- files/msc/script/Kreator.js
+++ files/msc/script/Kreator.js
@@ -4,7 +4,8 @@ var HordesOfChaos = [
 "<a href='https://docs.google.com/uc?export=open&id=1rM825C8nWU7z-1cpAAX9wimaxdSBPzHU'>War Curse</a>", 
 "<a href='https://docs.google.com/uc?export=open&id=1XL1iVT0kXU9CzKZdxrSOz2gAsXjyaXb-'>Escalation</a>", 
 "<a href='https://docs.google.com/uc?export=open&id=1Ut6qFM8iRt9BjICJ8cAEo5Qw1buDkyhm'>Amok Run</a>",
-"<a href='https://docs.google.com/uc?export=open&id=1zCcrSh91SzWaAPHkimi2AKYXzw3FWHlR'>Destroy What Destroys You</a>"
+"<a href='https://docs.google.com/uc?export=open&id=1zCcrSh91SzWaAPHkimi2AKYXzw3FWHlR'>Destroy What Destroys You</a>",
+"<a href='https://docs.google.com/uc?export=open&id=1-zlpUo3yitWZq3XMI1DCO9D-G5bUpgmr'>Radical Resistance</a>"
 ];
 document.getElementById("HordesOfChaos").innerHTML=HordesOfChaos.join(" <br> ");
 }
