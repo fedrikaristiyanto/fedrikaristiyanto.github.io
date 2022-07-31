ec1b11d64300108eaf04724d3902859a625bb71a
fedrikaristiyanto <mbahglendang@gmail.com>
2022-07-31 16:49:33 +0700

    Create big4.php

---
+++ files/msc/big4.php
@@ -0,0 +1,6 @@
+{ "records":[ 
+{"Band":"Anthrax","City":"New York","Country":"USA"}, 
+{"Band":"Megadeth","City":"Los Angeles","Country":"USA"}, 
+{"Band":"Metallica","City":"Los Angeles/San Francisco","Country":"USA"},
+{"Band":"Slayer","City":"Huntington Park","Country":"USA"}
+] }
