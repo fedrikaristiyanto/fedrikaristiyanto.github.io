cb2c8d4e42d7756279d5a7fdbe33ff759d6718aa
fedrikaristiyanto <mbahglendang@gmail.com>
2022-09-23 16:00:35 +0700

    Update Gorgoroth.js

--- files/msc/script/Gorgoroth.js
+++ files/msc/script/Gorgoroth.js
@@ -1,6 +1,7 @@
 var t = [
 "<a href='https://docs.google.com/uc?export=download&id=1YDvaySKkh5Jt-jJVyAI1N5gU9WOLq9_z'>Procreating Satan</a>",
-"<a href='https://docs.google.com/uc?export=download&id=18A0TiuYLg5PKF4bH6cpfQcEAMRHyaC-H'>Proclaiming Mercy - Damaging Instinct of Man</a>"
+"<a href='https://docs.google.com/uc?export=download&id=18A0TiuYLg5PKF4bH6cpfQcEAMRHyaC-H'>Proclaiming Mercy - Damaging Instinct of Man</a>",
+"<a href='https://docs.google.com/uc?export=download&id=1vOJMfaPcrsJGDxm4uu5gYnwtyQtEIznY'>Exit - Through Carved Stones</a>"
 ];
 document.getElementById("T0T1").innerHTML=t.join(" <br> ");
 
