b8505aee91ba4bce5ef4e30994adc32290e3b434
fedrikaristiyanto <mbahglendang@gmail.com>
2022-10-09 10:01:51 +0700

    Create HaloDuniaCanvas.js

---
+++ scripts/HaloDuniaCanvas.js
@@ -0,0 +1,12 @@
+document.body.onload=function(){HaloDuniaCanvas()};
+
+function HaloDuniaCanvas() {
+  var canvas = document.createElement("CANVAS");
+  var ctx = canvas.getContext("2d");
+  ctx.font = "32px monospace";
+  ctx.textAlign = "center";
+  ctx.textBaseline="middle"; 
+  ctx.fillStyle = "black";
+  ctx.fillText("Halo Dunia!", canvas.width/2, canvas.height/2);
+  document.body.appendChild(canvas);
+}
