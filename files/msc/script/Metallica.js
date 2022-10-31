a328a550a17270ab2cbbf00c73671f636c256c42
fedrikaristiyanto <mbahglendang@gmail.com>
2022-10-31 15:01:32 +0700

    Update Metallica.js

--- files/msc/script/Metallica.js
+++ files/msc/script/Metallica.js
@@ -1,5 +1,5 @@
 var mtlc = [
-"<a href='https://docs.google.com/uc?export=open&id=1-Az5FUdjWTR4k-BTXGXogvHBCNByJJa3'>Metallica - Enter Sandman</a>", 
-"<a href='https://docs.google.com/uc?export=open&id=1lZEwV7oyJySuSxnEZZeGtEUqienkapFi'>Metallica - Sad But True</a>"
+"<a href='https://docs.google.com/uc?export=download&id=1-Az5FUdjWTR4k-BTXGXogvHBCNByJJa3'>Metallica - Enter Sandman</a>", 
+"<a href='https://docs.google.com/uc?export=download&id=1lZEwV7oyJySuSxnEZZeGtEUqienkapFi'>Metallica - Sad But True</a>"
 ];
 document.getElementById("Mtlc").innerHTML=mtlc.join(" <br> ");
