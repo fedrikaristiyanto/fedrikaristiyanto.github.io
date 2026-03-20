52a31e9fd5fb2ce482dbec71c36a6bafa3ac43f7
fedrikaristiyanto <mbahglendang@gmail.com>
2026-03-20 11:35:52 +0700

    Create haloCanvas.js

---
+++ files/canvas/haloCanvas.js
@@ -0,0 +1,14 @@
+document.body.onload=function(){HaloCanvas()};
+
+function HaloCanvas() {
+  var canvas = document.createElement("CANVAS");
+    canvas.style.border="1px solid black";
+
+  var ctx = canvas.getContext("2d");
+    ctx.font = "32px monospace";
+    ctx.textAlign = "center";
+    ctx.textBaseline="middle"; 
+    ctx.fillStyle = "black";
+    ctx.fillText("Halo!", canvas.width/2, canvas.height/2);
+  document.body.appendChild(canvas);
+}
